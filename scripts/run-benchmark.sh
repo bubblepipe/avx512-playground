d3fb500cea522716abed3894ff894170600d8a1c
AOIDUO <aoiduo@riseup.com>
2022-11-13 21:44:18 +0000

    dynamic size benchmark

--- scripts/run-benchmark.sh
+++ scripts/run-benchmark.sh
@@ -5,14 +5,15 @@ normal=$(tput sgr0)
 
 # note: must enable -O3 flag to have vectorization 
 # native -mno-avx
+
 targets=('clang++-15 -O3 -march=native' 
          'clang++-15 -O3 -march=native -mno-avx -mno-sse' )
 
-
+params="-DBENCH"
 for cc in ${targets[@]}
 do
     echo ${bold}$cc${normal}
-    bash -c "$cc ./src/matrix-dynamic-size.cpp -std=c++11 -isystem benchmark/include  -L benchmark/build/src -l benchmark -lpthread -o /tmp/x" 
+    bash -c "$cc ./src/matrix-dynamic-size.cpp $params -std=c++11 -isystem benchmark/include  -L benchmark/build/src -l benchmark -lpthread -o /tmp/x" 
     # bash -c "$cc ./bench.cpp -std=c++11 -isystem benchmark/include  -L benchmark/build/src -l benchmark -lpthread -o /tmp/x" 
     echo ========================================================
     cc_t=`echo "$cc" | sed 's/ //g'`
--- src/matrix-dynamic-size.cpp
+++ src/matrix-dynamic-size.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <benchmark/benchmark.h>
+#include <stdlib.h>
 
 void print_matrix(std::vector<std::vector<float>> mat ){
     int row = mat.size();
@@ -41,8 +42,6 @@ void simplex_iterate(std::vector<std::vector<float>> & mat) {
         mat[piv_row][i] = mat[piv_row][i] / piv_elem;
     }
 
-    // print_matrix(mat);
-
     // step 0-3: row operations on remaining rows.
     for (int i = 0; i < row; i += 1) {
         if (i != piv_row) {
@@ -53,11 +52,12 @@ void simplex_iterate(std::vector<std::vector<float>> & mat) {
         }
     }
 
-    // print_matrix(mat);
-
+    #ifndef BENCH
+        print_matrix(mat);
+    #endif 
 }
 
-static void bench(benchmark::State &state) {
+static void bench_4_7(benchmark::State &state) {
     int row = 4;
     int col = 7;
     
@@ -80,12 +80,171 @@ static void bench(benchmark::State &state) {
         }
 
         simplex_iterate(m);
+        print_matrix(m);
+        simplex_iterate(m);
+        print_matrix(m);
+        exit(0);
+
+    }
+}
+
+static void bench_2(benchmark::State &state) {
+    int row = 6;
+    int col = 8;
+    
+    // maximize 12 x + 8 y 
+    // subject to:
+    // x + y < 80
+    // 2 x + 3 y < 60
+    // x + 3 y < 44
+    // 2 x + 5 y < 55
+    // 7 x + 3 y < 60
+    float mat_ref[6][8] =
+    {
+        //   x1  x2  x3  x4  x5  x6  x7  
+        {1 , 2 , 1 , 0 , 0 , 0 , 0 , 80},
+        {2 , 3 , 0 , 1 , 0 , 0 , 0 , 60},
+        {1 , 3 , 0 , 0 , 1 , 0 , 0 , 44},
+        {2 , 5 , 0 , 0 , 0 , 1 , 0 , 55},
+        {7 , 3 , 0 , 0 , 0 , 0 , 1 , 60},
+        {-40,-30,0 , 0 , 0 , 0 , 0 , 0},
+    };
+    
+    for (auto _ : state) {
+        std::vector<std::vector<float>> m;
+        m.resize(row, std::vector<float>(col, 0));
+    
+        for (int i = 0; i < row; i += 1) {
+            for (int j = 0; j < col; j += 1) {
+                m[i][j] = mat_ref[i][j];
+            }
+        }
+
+        simplex_iterate(m);
         simplex_iterate(m);
     }
+}
 
+std::vector<std::vector<float>> generate_random_matrix(int variable_count, int constraints_count) {
+    int row = constraints_count + 1;
+    int col = variable_count + constraints_count + 2;
+    std::vector<std::vector<float>> m;
+    m.resize(row, std::vector<float>(col, 0));
     
+    srand(114514);
+
+    // fill random number
+    for (int i = 0; i < constraints_count; i += 1){
+        for (int j = 0; j < variable_count; j += 1){
+            m[i][j] = rand() % 100;
+        }
+    }
+
+    // fill 1 
+    for (int i = 0; i < row; i += 1){
+        m[i][i + variable_count] = 1;
+    }
+
+    // fill rhs
+    for (int i = 0; i < row; i += 1){
+        m[i][col - 1] = rand() % 100;
+    }
+
+    // fill bottom row
+    for (int i = 0; i < variable_count; i += 1){
+        m[row-1][i] = -1 * (rand() % 100);
+    }
+
+    return m;
+}
+
+// int main(){
+//     auto m = generate_random_matrix(4,4);
+//     print_matrix(m);
+//     simplex_iterate(m); print_matrix(m);
+//     simplex_iterate(m); print_matrix(m);
+//     simplex_iterate(m); print_matrix(m);
+//     simplex_iterate(m); print_matrix(m);
+
+// }
+
+void bench_4(benchmark::State &state) {
+
+    int variable_count = 32;
+    int constraints_count = 32;
+    auto mat_ref= generate_random_matrix(variable_count, constraints_count);
+    int row = mat_ref.size();
+    int col = mat_ref[0].size();
+    
+    exit(0);
+    for (auto _ : state) {
+        std::vector<std::vector<float>> m;
+        m.resize(row, std::vector<float>(col, 0));
+    
+        for (int i = 0; i < row; i += 1) {
+            for (int j = 0; j < col; j += 1) {
+                m[i][j] = mat_ref[i][j];
+            }
+        }
+
+        simplex_iterate(m); // print_matrix(m);
+        simplex_iterate(m); // print_matrix(m);
+        simplex_iterate(m); // print_matrix(m);
+        simplex_iterate(m); // print_matrix(m);
+        simplex_iterate(m); // print_matrix(m);
+        simplex_iterate(m); // print_matrix(m);
+        simplex_iterate(m); // print_matrix(m);
+        simplex_iterate(m); // print_matrix(m);
+        // exit(0);
+    }
 }
 
 
-BENCHMARK(bench);
+static void bench_3(benchmark::State &state) {
+    int row = 8;
+    int col = 13;
+        // maximize x1 + 2x2 + 3x3 + 4x4 
+    // subject to 
+    // 2 x1 + 3 x2 + 4 x3 + 5 x4 < 1000
+    // 7 x1 + 2 x4 < 8000
+    // 3 x1 + 1 x4 < 800
+    // 11 x2 + 12x3 < 7777
+
+    float mat_ref[8][13] =
+    {
+        //x1 x2  x3  x4  s1  s2  s3  s4  s5  s6  s7  P   rhs
+        {2 , 3 , 4 , 5 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 8000},
+        {7 , 2 , 3 , 4 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 6000},
+        {3 , 1 , 2 , 2 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 4400},
+        {11, 12, 13, 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 78600},
+        {13, 14, 15, 16, 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 81000},
+        {21, 22, 23, 24, 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 19190},
+        {81, 9 , 3 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 114514},
+        {-40,-30,-10,-8, 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0},
+    };
+    
+    for (auto _ : state) {
+        std::vector<std::vector<float>> m;
+        m.resize(row, std::vector<float>(col, 0));
+    
+        for (int i = 0; i < row; i += 1) {
+            for (int j = 0; j < col; j += 1) {
+                m[i][j] = mat_ref[i][j];
+            }
+        }
+
+        simplex_iterate(m); // print_matrix(m);
+        simplex_iterate(m); // print_matrix(m);
+        simplex_iterate(m); // print_matrix(m);
+        simplex_iterate(m); // print_matrix(m);
+        simplex_iterate(m); // print_matrix(m);
+        simplex_iterate(m); // print_matrix(m);
+        simplex_iterate(m); // print_matrix(m);
+        simplex_iterate(m); // print_matrix(m);
+        // exit(0);
+    }
+}
+
+
+BENCHMARK(bench_4);
 BENCHMARK_MAIN();
