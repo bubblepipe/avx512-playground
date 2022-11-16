0769800bb87cc0efab0a17cad4e5baba419f9b4d
AOIDUO <aoiduo@riseup.com>
2022-11-16 19:42:59 +0000

    delete binary

--- src/matrix-dynamic-size.cpp
+++ src/matrix-dynamic-size.cpp
@@ -1,61 +1,5 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <benchmark/benchmark.h>
-#include <stdlib.h>
-
-void print_matrix(std::vector<std::vector<float>> mat ){
-    int row = mat.size();
-    int col = mat[0].size();
-
-    for (int i = 0; i < row; i += 1) {
-        for (int j = 0; j < col; j += 1) {
-            std::cout << mat[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
-}
-
-void simplex_iterate(std::vector<std::vector<float>> & mat) {
-
-    int row = mat.size();
-    int col = mat[0].size();
-
-    // Step 0-1: Picking the Pivot Elem
-    auto x = mat[row - 1];
-    auto piv_col_it = std::min_element(std::begin(mat[row - 1]), std::end(mat[row - 1]));
-    int piv_col = std::distance(std::begin(mat[row - 1]), piv_col_it);
-
-    std::vector<float> ratios(row - 1);
-    for (int i = 0; i < row - 1; i += 1) {
-        ratios[i] = mat[i][col - 1] / mat[i][piv_col];
-    }
-    auto piv_row_it = std::min_element(std::begin(ratios), std::end(ratios));
-    int piv_row = std::distance(std::begin(ratios), piv_row_it);
-
-    auto piv_elem = mat[piv_row][piv_col];
-    // std::cout << piv_elem << std::endl << std::endl;
-
-    // step 0-2: div piv row by piv item:
-    for (int i = 0; i < col; i += 1) {
-        mat[piv_row][i] = mat[piv_row][i] / piv_elem;
-    }
-
-    // step 0-3: row operations on remaining rows.
-    for (int i = 0; i < row; i += 1) {
-        if (i != piv_row) {
-            auto xxx = mat[i][piv_col];
-            for (int j = 0; j < col; j += 1) {
-                mat[i][j] = mat[i][j] - xxx * mat[piv_row][j];
-            }
-        }
-    }
-
-    #ifndef BENCH
-        print_matrix(mat);
-    #endif 
-}
+#include <matrix-dynamic-size.h>
+// #include <"src/matrix-dynamic-size.h">
 
 static void bench_4_7(benchmark::State &state) {
     int row = 4;
@@ -80,15 +24,12 @@ static void bench_4_7(benchmark::State &state) {
         }
 
         simplex_iterate(m);
-        print_matrix(m);
         simplex_iterate(m);
-        print_matrix(m);
-        exit(0);
 
     }
 }
 
-static void bench_2(benchmark::State &state) {
+static void bench_6_8(benchmark::State &state) {
     int row = 6;
     int col = 8;
     
@@ -122,40 +63,9 @@ static void bench_2(benchmark::State &state) {
 
         simplex_iterate(m);
         simplex_iterate(m);
+        simplex_iterate(m);
+        simplex_iterate(m);
     }
-}
-
-std::vector<std::vector<float>> generate_random_matrix(int variable_count, int constraints_count) {
-    int row = constraints_count + 1;
-    int col = variable_count + constraints_count + 2;
-    std::vector<std::vector<float>> m;
-    m.resize(row, std::vector<float>(col, 0));
-    
-    srand(114514);
-
-    // fill random number
-    for (int i = 0; i < constraints_count; i += 1){
-        for (int j = 0; j < variable_count; j += 1){
-            m[i][j] = rand() % 100;
-        }
-    }
-
-    // fill 1 
-    for (int i = 0; i < row; i += 1){
-        m[i][i + variable_count] = 1;
-    }
-
-    // fill rhs
-    for (int i = 0; i < row; i += 1){
-        m[i][col - 1] = rand() % 100;
-    }
-
-    // fill bottom row
-    for (int i = 0; i < variable_count; i += 1){
-        m[row-1][i] = -1 * (rand() % 100);
-    }
-
-    return m;
 }
 
 // int main(){
@@ -168,47 +78,9 @@ std::vector<std::vector<float>> generate_random_matrix(int variable_count, int c
 
 // }
 
-void bench_4(benchmark::State &state) {
-
-    int variable_count = 32;
-    int constraints_count = 32;
-    auto mat_ref= generate_random_matrix(variable_count, constraints_count);
-    int row = mat_ref.size();
-    int col = mat_ref[0].size();
-    
-    exit(0);
-    for (auto _ : state) {
-        std::vector<std::vector<float>> m;
-        m.resize(row, std::vector<float>(col, 0));
-    
-        for (int i = 0; i < row; i += 1) {
-            for (int j = 0; j < col; j += 1) {
-                m[i][j] = mat_ref[i][j];
-            }
-        }
-
-        simplex_iterate(m); // print_matrix(m);
-        simplex_iterate(m); // print_matrix(m);
-        simplex_iterate(m); // print_matrix(m);
-        simplex_iterate(m); // print_matrix(m);
-        simplex_iterate(m); // print_matrix(m);
-        simplex_iterate(m); // print_matrix(m);
-        simplex_iterate(m); // print_matrix(m);
-        simplex_iterate(m); // print_matrix(m);
-        // exit(0);
-    }
-}
-
-
-static void bench_3(benchmark::State &state) {
+static void bench_8_13(benchmark::State &state) {
     int row = 8;
     int col = 13;
-        // maximize x1 + 2x2 + 3x3 + 4x4 
-    // subject to 
-    // 2 x1 + 3 x2 + 4 x3 + 5 x4 < 1000
-    // 7 x1 + 2 x4 < 8000
-    // 3 x1 + 1 x4 < 800
-    // 11 x2 + 12x3 < 7777
 
     float mat_ref[8][13] =
     {
@@ -246,5 +118,36 @@ static void bench_3(benchmark::State &state) {
 }
 
 
-BENCHMARK(bench_4);
+void bench_x(benchmark::State &state) {
+
+    int variable_count = 1024;
+    int constraints_count = variable_count;
+    auto mat_ref= generate_random_matrix(variable_count, constraints_count);
+    int row = mat_ref.size();
+    int col = mat_ref[0].size();
+    
+    for (auto _ : state) {
+        std::vector<std::vector<float>> m;
+        m.resize(row, std::vector<float>(col, 0));
+    
+        for (int i = 0; i < row; i += 1) {
+            for (int j = 0; j < col; j += 1) {
+                m[i][j] = mat_ref[i][j];
+            }
+        }
+
+        simplex_iterate(m); // print_matrix(m);
+        simplex_iterate(m); // print_matrix(m);
+        simplex_iterate(m); // print_matrix(m);
+        simplex_iterate(m); // print_matrix(m);
+        simplex_iterate(m); // print_matrix(m);
+        simplex_iterate(m); // print_matrix(m);
+        simplex_iterate(m); // print_matrix(m);
+        simplex_iterate(m); // print_matrix(m);
+        // exit(0);
+    }
+}
+
+BENCHMARK(bench_6_8);
+// BENCHMARK(bench_x);
 BENCHMARK_MAIN();
---
+++ src/matrix-dynamic-size.h
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <benchmark/benchmark.h>
+#include <stdlib.h>
+
+void print_matrix(std::vector<std::vector<float>> mat ){
+    int row = mat.size();
+    int col = mat[0].size();
+
+    for (int i = 0; i < row; i += 1) {
+        for (int j = 0; j < col; j += 1) {
+            std::cout << mat[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+void simplex_iterate(std::vector<std::vector<float>> & mat) {
+
+    int row = mat.size();
+    int col = mat[0].size();
+
+    // Step 0-1: Picking the Pivot Elem
+    auto x = mat[row - 1];
+    auto piv_col_it = std::min_element(std::begin(mat[row - 1]), std::end(mat[row - 1]));
+    int piv_col = std::distance(std::begin(mat[row - 1]), piv_col_it);
+
+    std::vector<float> ratios(row - 1);
+    for (int i = 0; i < row - 1; i += 1) {
+        ratios[i] = mat[i][col - 1] / mat[i][piv_col];
+    }
+    auto piv_row_it = std::min_element(std::begin(ratios), std::end(ratios));
+    int piv_row = std::distance(std::begin(ratios), piv_row_it);
+
+    auto piv_elem = mat[piv_row][piv_col];
+    // std::cout << piv_elem << std::endl << std::endl;
+
+    // step 0-2: div piv row by piv item:
+    for (int i = 0; i < col; i += 1) {
+        mat[piv_row][i] = mat[piv_row][i] / piv_elem;
+    }
+
+    // step 0-3: row operations on remaining rows.
+    for (int i = 0; i < row; i += 1) {
+        if (i != piv_row) {
+            auto xxx = mat[i][piv_col];
+            for (int j = 0; j < col; j += 1) {
+                mat[i][j] = mat[i][j] - xxx * mat[piv_row][j];
+            }
+        }
+    }
+
+    #ifndef BENCH
+        print_matrix(mat);
+    #endif 
+}
+
+std::vector<std::vector<float>> generate_random_matrix(int variable_count, int constraints_count) {
+    int row = constraints_count + 1;
+    int col = variable_count + constraints_count + 2;
+    std::vector<std::vector<float>> m;
+    m.resize(row, std::vector<float>(col, 0));
+    
+    srand(114514);
+
+    // fill random number
+    for (int i = 0; i < constraints_count; i += 1){
+        for (int j = 0; j < variable_count; j += 1){
+            m[i][j] = rand() % 100;
+        }
+    }
+
+    // fill 1 
+    for (int i = 0; i < row; i += 1){
+        m[i][i + variable_count] = 1;
+    }
+
+    // fill rhs
+    for (int i = 0; i < row; i += 1){
+        m[i][col - 1] = rand() % 100;
+    }
+
+    // fill bottom row
+    for (int i = 0; i < variable_count; i += 1){
+        m[row-1][i] = -1 * (rand() % 100);
+    }
+
+    return m;
+}
