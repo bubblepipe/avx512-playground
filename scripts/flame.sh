2dc1bfffefeb4ebc70d81012db821e803a4eeb21
AOIDUO <aoiduo@riseup.com>
2023-01-30 00:48:28 +0000

    immintrin

--- scripts/flame.sh
+++ scripts/flame.sh
@@ -4,7 +4,7 @@ sudo perf script > out.perf
 sudo chown aoiduo:aoiduo out.perf
 ./FlameGraph/stackcollapse-perf.pl ./out.perf > out.flame
 ./FlameGraph/flamegraph.pl  --width 2400 ./out.flame > flame.svg
-sudo rm /dev/shm/1145141919810  
+sudo chown aoiduo:aoiduo /dev/shm/1145141919810  
 sudo rm ./out.flame 
 sudo rm ./out.perf
 sudo rm *.old
--- src/bench_mat/flat-int.cpp
+++ src/bench_mat/flat-int.cpp
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <bench_vec/bench_vector_int.hpp>
+#include <immintrin.h>
 
 class matrix {
 
@@ -47,14 +48,8 @@ void mat_add ( unsigned int row, unsigned int col,
     int32_t * dst_ptr  = (int32_t *) mat_dst.m.data();
     vec_add_莎莎(size, src1_ptr, src2_ptr, dst_ptr);
 
-    // for (int i = 0; i < row; i += 1) {
-    //     for (int j = 0; j < col; j += 1) {
-    //         int32_t src1 = mat_src1.get(i,j);
-    //         int32_t src2 = mat_src2.get(i,j);
-    //         mat_dst.set(i,j,  src1 + src2);
-    //     }
-    // }
 }
+
 void mat_add_manual ( unsigned int row, unsigned int col, 
     matrix & mat_src1, matrix & mat_src2, matrix & mat_dst ) {
     
@@ -68,14 +63,13 @@ void mat_add_manual ( unsigned int row, unsigned int col,
     int32_t * src2_ptr = (int32_t *) mat_src2.m.data();
     int32_t * dst_ptr  = (int32_t *) mat_dst.m.data();
 
-    vec_add_simdpp(size, src1_ptr, src2_ptr, dst_ptr);
-    
-    // for (int32_t i = 0; i < size; i += 8 ){
-        // simdpp::int32<8> src1_ymm = simdpp::load(src1_ptr + i);
-        // simdpp::int32<8> src2_ymm = simdpp::load(src2_ptr + i);
-        // auto dst_new_ymm = simdpp::add(src1_ymm, src2_ymm);
-        // simdpp::store(dst_ptr + i, dst_new_ymm );                                         
-    // }
+    for (int32_t i = 0; i < size; i += 8 ){
+        __m256 src1 = _mm256_loadu_si256 ((__m256i_u*) (src1_ptr + i));
+        __m256 src2 = _mm256_loadu_si256 ((__m256i_u*) (src2_ptr + i));
+        __m256 result = _mm256_add_epi32(src1, src2);
+        _mm256_storeu_si256((__m256i_u*) (dst_ptr + i), result);
+    }
+
 }
 
 
@@ -87,17 +81,14 @@ void mat_fma_manual ( unsigned int row, unsigned int col,
     int32_t * dst_ptr  = (int32_t *) mat_dst.m.data();
 
     for (int32_t i = 0; i < size; i += 8 ){
-        simdpp::int32<8> src1_ymm = simdpp::load(src1_ptr + i);
-        simdpp::int32<8> src2_ymm = simdpp::load(src2_ptr + i);
-        simdpp::int32<8> src3_ymm = simdpp::load(dst_ptr + i);
+        __m256 src1 = _mm256_loadu_si256 ((__m256i_u*) (src1_ptr + i));
+        __m256 src2 = _mm256_loadu_si256 ((__m256i_u*) (src2_ptr + i));
+        __m256 r1 = _mm256_mullo_epi32(src1, src2);
         
-        // auto dst_new_ymm = simdpp::add(src1_ymm, src2_ymm);
+        __m256 src3 = _mm256_loadu_si256 ((__m256i_u*) (dst_ptr + i));
 
-        auto tmp = simdpp::mul_lo(src1_ymm, src2_ymm);
-        auto dst_new_ymm = simdpp::add(tmp, src3_ymm);
-
-        simdpp::store(dst_ptr + i, dst_new_ymm );                                         
-
+        __m256 r2 = _mm256_add_epi32(r1, src3);
+        _mm256_storeu_si256((__m256i_u*) (dst_ptr + i), r2);
     }
 }
 
@@ -115,9 +106,9 @@ static void flat(benchmark::State& state,
 
     for (int r = 0; r < row; r += 1) {
         for (int c = 0; c < col; c += 1) {
-            mat_src1.set(r,c,  rand() );
-            mat_src2.set(r,c,  rand() );
-            mat_dst. set(r,c,  rand() );
+            mat_src1.set(r,c, r); // rand() );
+            mat_src2.set(r,c, c); // rand() );
+            mat_dst. set(r,c, r); // rand() );
             mat_dst_ref.set(r,c, mat_dst.get(r,c));
         }
     }
