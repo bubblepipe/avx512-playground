515d645488658d19442336635a70e9be648c834d
Your Name <you@example.com>
2023-02-28 22:23:26 +0000

    macro for avx intrinsic

--- src/bench_pivot/pivot.cpp
+++ src/bench_pivot/pivot.cpp
@@ -73,56 +73,44 @@ bool pivot(matrix<T> & mat, unsigned pivotRow, unsigned pivotCol) {
     T * rowPtr = mat.getRowPtr(rowIndex);
     rowPtr[0] *= pivotRowPtr[0];
 
-    // T index0 = rowPtr[0];
     // row = row * ConstA +  ConstB * PivotRow;
     if constexpr (std::is_same<T, double>::value) {
-      typedef doubleZmm T2_Zmm;
-      T2_Zmm ConstA = pivotRowPtr[0];
-      T2_Zmm ConstC = rowPtr[pivotCol];
+      typedef doubleZmm Zmm;
+      CONST_A_C
       unsigned colIndex = 0;
-      __m512d mat_row_ymm = _mm512_load_pd((const T *)(rowPtr + colIndex));
-      __m512d result0 = _mm512_mul_pd(mat_row_ymm, ConstA);
-      __m512d pivot_row_ymm = _mm512_load_pd((const T *)(pivotRowPtr + colIndex));
-      __m512d result1 = _mm512_fmadd_pd(ConstC, pivot_row_ymm, result0);
-      _mm512_mask_store_pd((T *)(rowPtr + colIndex), 0b11111110,result1);
+      F32_64_OP(__m512d, _mm512_load_pd, _mm512_mul_pd, _mm512_fmadd_pd)
+      F64_MASK_STORE
       for (colIndex = ZmmDoubleVecSize; colIndex < nCol; colIndex += ZmmDoubleVecSize) {
-        __m512d mat_row_ymm = _mm512_load_pd((const T *)(rowPtr + colIndex));
-        __m512d result0 = _mm512_mul_pd(mat_row_ymm, ConstA);
-        __m512d pivot_row_ymm = _mm512_load_pd((const T *)(pivotRowPtr + colIndex));
-        __m512d result1 = _mm512_fmadd_pd(ConstC, pivot_row_ymm, result0);
-        _mm512_store_pd((T *)(rowPtr + colIndex), result1);
+        F32_64_OP(__m512d, _mm512_load_pd, _mm512_mul_pd, _mm512_fmadd_pd)
+        F32_64_STORE(_mm512_store_pd)
       }
     }
     else if constexpr (std::is_same<T, float>::value) {
-      typedef floatZmm T2_Zmm;
-      T2_Zmm ConstA = pivotRowPtr[0];
-      T2_Zmm ConstC = rowPtr[pivotCol];
-      for (unsigned colIndex = 0; colIndex < nCol; colIndex += ZmmFloatVecSize) {
-        __m512 mat_row_ymm = _mm512_load_ps((const T *)(rowPtr + colIndex));
-        __m512 result0 = _mm512_mul_ps(mat_row_ymm, ConstA);
-        __m512 pivot_row_ymm = _mm512_load_ps((const T *)(pivotRowPtr + colIndex));
-        __m512 result1 = _mm512_fmadd_ps(ConstC, pivot_row_ymm, result0);
-        _mm512_store_ps((T *)(rowPtr + colIndex), result1);
+      typedef floatZmm Zmm;
+      CONST_A_C
+      unsigned colIndex = 0;
+      F32_64_OP(__m512, _mm512_load_ps, _mm512_mul_ps, _mm512_fmadd_ps)
+      F32_MASK_STORE
+      for (colIndex = ZmmFloatVecSize; colIndex < nCol; colIndex += ZmmFloatVecSize) {
+        F32_64_OP(__m512, _mm512_load_ps, _mm512_mul_ps, _mm512_fmadd_ps)
+        F32_64_STORE(_mm512_store_ps)
       }
     } 
     else if constexpr (std::is_same<T, int16_t>::value) {
-      typedef int16Zmm T2_Zmm;
-      T2_Zmm ConstA = pivotRowPtr[0];
-      T2_Zmm ConstC = rowPtr[pivotCol];
-      for (unsigned colIndex = 0; colIndex < nCol; colIndex += ZmmInt16VecSize) {
-        __m512 mat_row_ymm = _mm512_load_si512((const T *)(rowPtr + colIndex));
-        __m512 result0 = mul<true>(mat_row_ymm, ConstA);
-        __m512 pivot_row_ymm = _mm512_load_si512((const T *)(pivotRowPtr + colIndex));
-        __m512 result1 = mul<true>(ConstC, pivot_row_ymm);
-        __m512 result2 = add<true>(result1, result0);
-        _mm512_store_si512((T *)(rowPtr + colIndex), result2);
+      T index0 = rowPtr[0]; // manual backup of the first element
+      typedef int16Zmm Zmm;
+      CONST_A_C
+      unsigned colIndex = 0;
+      for (colIndex = 0; colIndex < nCol; colIndex += ZmmInt16VecSize) {
+        I16_OP
+        I16_TORE
       }
+      rowPtr[0] = index0;
     }
     else {
       printf("neither USE_INT16, USE_INT23 not USE_INT52 is defined in pivot");
       exit(0);
     }
-    // rowPtr[0] = index0;
     // mat(rowIndex, pivotCol) = mat(rowIndex, pivotCol);
     mat(rowIndex, pivotCol) = pivotColBackup;
     mat(rowIndex, pivotCol) *= pivotRowPtr[pivotCol];
