559ba9c1ecdb0fb126fcc84a09a6c8e763a7231f
AOIDUO <aoiduo@riseup.com>
2023-03-06 18:09:13 +0000

    scripts/plot_pivot_check_unchecked.py

--- scripts/plot_pivot_check_unchecked.py
+++ scripts/plot_pivot_check_unchecked.py
@@ -6,7 +6,7 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import sys
 import numpy as np
 
-filename = sys.argv[1]
+filename = './verified-plot/plot-pivot-checked_check_unchecked'
 # size = int(sys.argv[2])
 
 f = open(filename, "r")
@@ -24,17 +24,16 @@ for x in xs:
     elif x == '\n':
         pass
     else:
-        cpu_time = float(x.split()[3]) # ['bench_matrix_flatvector/add/16/16', '198', 'ns', '198', 'ns', '3414984']
-        xx = x.split()[0].split('/') # ['bench_matrix_flatvector', 'add', '16', '16']
-        bench_name = f'{xx[0]}/{xx[1]}'
-        row = float(xx[2])
-        col = float(xx[3])
+        cpu_time = float(x.split()[3]) # check/double           25.4 ns         25.4 ns     28695205
+        xx = x.split()[0].split('/') 
+        bench_name = f'{xx[0]}'
+        arg = xx[1]
         if not (bench_name in d):
             d[bench_name] = {}
-        if (row,col) in d[bench_name]:
-            d[bench_name][row,col].append(cpu_time)
+        if arg in d[bench_name]:
+            d[bench_name][arg].append(cpu_time)
         else:
-            d[bench_name][row,col] = [cpu_time]
+            d[bench_name][arg] = [cpu_time]
 
 
 color_iter = iter(['slateblue', 'crimson', 'lightseagreen', 'darkred'])
@@ -43,40 +42,36 @@ fig, ax = plt.subplots(figsize =(16, 9))
 
 barWidth = 0.2
 xss = []
-xss.append(np.arange(3))
+xss.append(np.arange(4))
 yss = []
 errss = []
-xlabel = ['col size 8','col size 16','col size 32']
-
+xlabel = ['int16 (vectorized)', 'float (vectorized)', 'double (vectorized)', 'MPInt (scalcar)']
+legends = ['overflow check disabled', 'overflow check enabled']
+title = 'pivot, row 30, col 16, no normalize'
 for bench_name, bench_vals in d.items():
 
     xss.append([x + barWidth for x in xss[-1]])
     ys = []
     errs = []
-    for (row,col), (cpu_time_xs) in bench_vals.items():
+    for arg, (cpu_time_xs) in bench_vals.items():
         ys.append(sum(cpu_time_xs)/len(cpu_time_xs))
         errs.append(np.std(cpu_time_xs))
     yss.append(ys)
     errss.append(errs)
 
 for (xs,ys,errs) in zip(xss,yss,errss):
+    print(xs)
+    print(ys)
     ax.bar(xs,ys,width=barWidth)
-# ax.legend(labels=['int16', 'int32', 'float', 'double'], fontsize="x-large")
-# ax.legend(labels=['int16', 'int32', 'int16 with overflow checking', 'int32 with overflow checking'], fontsize="x-large")
-# ax.legend(labels=['float', 'double', 'float with overflow checking', 'double with overflow checking'], fontsize="x-large")
-# ax.legend(labels=['unaligned', 'aligned'], fontsize="x-large")
-ax.legend(labels=['int16', 'float', 'double', 'MPInt'], fontsize="x-large")
+
+ax.legend(labels=legends, fontsize="x-large")
 
 for (xs,ys,errs) in zip(xss,yss,errss):
     color = next(color_iter)
     ax.errorbar(xs,ys,yerr=errs,fmt="|",elinewidth=2)
 
 
-# exit(0)
-
-
-# plt.xticks([ x+0.5*barWidth for x in xss[1]], list(xlabel), fontsize="x-large")
 plt.xticks([ x-0.5*barWidth for x in xss[1]], list(xlabel), fontsize="x-large")
 ax.set_ylabel("time (ns), lower is better", fontsize="x-large")
-ax.set_title(filename, fontsize="xx-large")
+ax.set_title(title, fontsize="xx-large")
 plt.show()
