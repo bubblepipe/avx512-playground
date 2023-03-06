6101371dd026bdc3e606ad3202e28e9e5a7e4472
Your Name <you@example.com>
2023-03-06 22:01:50 +0000

    feclearexcept_local_sse

--- src/utils/cfenv_local.cpp
+++ src/utils/cfenv_local.cpp
@@ -31,8 +31,57 @@ feenableexcept_local(int mask)
 	return (omask);
 }
 
-int
-fetestexcept_local(int excepts)
+// from 
+// https://github.com/openbsd/src/blob/master/sys/arch/amd64/include/fenv.h
+typedef	struct {
+	struct {
+		unsigned int __control;		/* Control word register */
+		unsigned int __status;		/* Status word register */
+		unsigned int __tag;		/* Tag word register */
+		unsigned int __others[4];	/* EIP, Pointer Selector, etc */
+	} __x87;
+	unsigned int __mxcsr;			/* Control, status register */
+} fenv_t_local;
+
+
+inline int feclearexcept_local_x87(int excepts) {
+	excepts &= FE_ALL_EXCEPT;
+	fenv_t_local fenv;
+	__asm__ volatile ("fnstenv %0" : "=m" (fenv));
+	fenv.__x87.__status &= ~excepts;
+	__asm__ volatile ("fldenv %0" : : "m" (fenv));
+	return (0);
+}
+
+
+inline int feclearexcept_local_sse(int excepts) {
+	unsigned int mxcsr;
+	__asm__ volatile ("stmxcsr %0" : "=m" (mxcsr));
+	mxcsr &= ~excepts;
+	__asm__ volatile ("ldmxcsr %0" : : "m" (mxcsr));
+	return (0);
+}
+inline int feclearexcept_local(int excepts) {
+
+	excepts &= FE_ALL_EXCEPT;
+
+	fenv_t_local fenv;
+	/* Store the current x87 floating-point environment */
+	__asm__ volatile ("fnstenv %0" : "=m" (fenv));
+	/* Clear the requested floating-point exceptions */
+	fenv.__x87.__status &= ~excepts;
+	/* Load the x87 floating-point environment */
+	__asm__ volatile ("fldenv %0" : : "m" (fenv));
+
+	unsigned int mxcsr;
+	__asm__ volatile ("stmxcsr %0" : "=m" (mxcsr));
+	mxcsr &= ~excepts;
+	__asm__ volatile ("ldmxcsr %0" : : "m" (mxcsr));
+
+	return (0);
+}
+
+inline int fetestexcept_local(int excepts)
 {
 	// Copied from https://github.com/openbsd/src/blob/master/lib/libm/arch/amd64/fenv.c
 	unsigned short status = 0;
