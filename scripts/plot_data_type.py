1de513cf9010646d4c79ba63d71a933c1484fdd3
Your Name <you@example.com>
2023-04-12 16:12:54 +0100

    remove title

--- scripts/plot_data_type.py
+++ scripts/plot_data_type.py
@@ -99,5 +99,5 @@ for (xs,ys,errs) in zip(xss,yss,errss):
 plt.xticks([  x-0.5*barWidth for x in xss[1] ], list(xlabel), fontsize=size)
 ax.set_ylabel("CPU Time (ns), lower is better", fontsize=size)
 # ax.set_xlabel(x_axis_label, fontsize=size)
-ax.set_title(title, fontsize=size)
+# ax.set_title(title, fontsize=size)
 plt.savefig( filename +'.png', dpi=300)
